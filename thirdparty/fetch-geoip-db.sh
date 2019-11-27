cd427b2d3c870c9c7207ff733f4afdaa506cffa1
CastleJing <1145754507@qq.com>
2019-11-28 00:52:56 +0800

    更改第三方依赖为分流仓库

--- thirdparty/fetch-geoip-db.sh
+++ thirdparty/fetch-geoip-db.sh
@@ -12,11 +12,11 @@ filename="GeoLite2-Country.mmdb.gz"
 # Database does not exist or is older than 30 days.
 if [ ! -e $filename ] || [ -n "$(find . -name $filename -mtime +30 -print)" ]; then
 	rm -f $filename
-	echo "Updating GeoIP country database from MaxMind."
+	echo "Updating GeoIP country database from Gitee."
 	if command -v curl >/dev/null 2>&1; then
-		curl -s -L -O http://geolite.maxmind.com/download/geoip/database/$filename
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/$filename
 	else
-		wget -cq http://geolite.maxmind.com/download/geoip/database/$filename
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/$filename
 	fi
 fi
 
--- thirdparty/fetch-thirdparty-deps.sh
+++ thirdparty/fetch-thirdparty-deps.sh
@@ -14,83 +14,122 @@ mkdir -p "${download_dir}"
 cd "${download_dir}" || exit 1
 
 if [ ! -f ICSharpCode.SharpZipLib.dll ]; then
-	echo "Fetching ICSharpCode.SharpZipLib from NuGet"
-	../noget.sh SharpZipLib 1.1.0
-	cp ./SharpZipLib/lib/net45/ICSharpCode.SharpZipLib.dll .
-	rm -rf SharpZipLib
+	echo "Fetching ICSharpCode.SharpZipLib from Gitee."
+	if command -v curl >/dev/null 2>&1; then
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/ICSharpCode.SharpZipLib.dll
+	else
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/ICSharpCode.SharpZipLib.dll
+	fi
 fi
 
 if [ ! -f MaxMind.Db.dll ]; then
-	echo "Fetching MaxMind.Db from NuGet"
-	../noget.sh MaxMind.Db 2.0.0 -IgnoreDependencies
-	cp ./MaxMind.Db/lib/net45/MaxMind.Db.* .
-	rm -rf MaxMind.Db
+	echo "Fetching MaxMind.Db from Gitee."
+	if command -v curl >/dev/null 2>&1; then
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/MaxMind.Db.dll
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/MaxMind.Db.xml
+	else
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/MaxMind.Db.dll
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/MaxMind.Db.xml
+	fi
 fi
 
 if [ ! -f nunit.framework.dll ]; then
-	echo "Fetching NUnit from NuGet"
-	../noget.sh NUnit 3.0.1
-	cp ./NUnit/lib/net40/nunit.framework* .
-	rm -rf NUnit
+	echo "Fetching NUnit from Gitee."
+	if command -v curl >/dev/null 2>&1; then
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent-x86.exe
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent-x86.exe.config
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent.exe
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent.exe.config
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.addin.xml
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.addins
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.api.dll
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.api.xml
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.dll
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.framework.dll
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.framework.xml
+	else
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent-x86.exe
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent-x86.exe.config
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent.exe
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit-agent.exe.config
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.addin.xml
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.addins
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.api.dll
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.api.xml
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.engine.dll
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.framework.dll
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit.framework.xml
+	fi
 fi
 
 if [ ! -f nunit3-console.exe ]; then
-	echo "Fetching NUnit.Console from NuGet"
-	../noget.sh NUnit.Console 3.0.1
-	cp -R ./NUnit.Console/tools/* .
-	chmod +x nunit3-console.exe
-	rm -rf NUnit.Console
+	echo "Fetching NUnit.Console from Gitee."
+	if command -v curl >/dev/null 2>&1; then
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit3-console.exe
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit3-console.exe.config
+	else
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit3-console.exe
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/nunit3-console.exe.config
+	fi
 fi
 
 if [ ! -f Open.Nat.dll ]; then
-	echo "Fetching Open.Nat from NuGet"
-	../noget.sh Open.Nat 2.1.0
-	if [ -d ./Open.NAT ]; then mv Open.NAT Open.Nat; fi
-	cp ./Open.Nat/lib/net45/Open.Nat.dll .
-	rm -rf Open.Nat
+	echo "Fetching Open.Nat from Gitee."
+	if command -v curl >/dev/null 2>&1; then
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/Open.Nat.dll
+	else
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/Open.Nat.dll
+	fi
 fi
 
 if [ ! -f FuzzyLogicLibrary.dll ]; then
-	echo "Fetching FuzzyLogicLibrary from NuGet."
-	../noget.sh FuzzyLogicLibrary 1.2.0
-	cp ./FuzzyLogicLibrary/bin/Release/FuzzyLogicLibrary.dll .
-	rm -rf FuzzyLogicLibrary
+	echo "Fetching FuzzyLogicLibrary from Gitee."
+	if command -v curl >/dev/null 2>&1; then
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/FuzzyLogicLibrary.dll
+	else
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/FuzzyLogicLibrary.dll
+	fi
 fi
 
 if [ ! -f SDL2-CS.dll ] || [ ! -f SDL2-CS.dll.config ]; then
-	echo "Fetching SDL2-CS from GitHub."
+	echo "Fetching SDL2-CS from Gitee."
 	if command -v curl >/dev/null 2>&1; then
-		curl -s -L -O https://github.com/OpenRA/SDL2-CS/releases/download/20190907/SDL2-CS.dll
-		curl -s -L -O https://github.com/OpenRA/SDL2-CS/releases/download/20190907/SDL2-CS.dll.config
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/SDL2-CS.dll
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/SDL2-CS.dll.config
 	else
-		wget -cq https://github.com/OpenRA/SDL2-CS/releases/download/20190907/SDL2-CS.dll
-		wget -cq https://github.com/OpenRA/SDL2-CS/releases/download/20190907/SDL2-CS.dll.config
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/SDL2-CS.dll
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/SDL2-CS.dll.config
 	fi
 fi
 
 if [ ! -f OpenAL-CS.dll ] || [ ! -f OpenAL-CS.dll.config ]; then
-	echo "Fetching OpenAL-CS from GitHub."
+	echo "Fetching OpenAL-CS from Gitee."
 	if command -v curl >/dev/null 2>&1; then
-		curl -s -L -O https://github.com/OpenRA/OpenAL-CS/releases/download/20190907/OpenAL-CS.dll
-		curl -s -L -O https://github.com/OpenRA/OpenAL-CS/releases/download/20190907/OpenAL-CS.dll.config
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/OpenAL-CS.dll
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/OpenAL-CS.dll.config
 	else
-		wget -cq https://github.com/OpenRA/OpenAL-CS/releases/download/20190907/OpenAL-CS.dll
-		wget -cq https://github.com/OpenRA/OpenAL-CS/releases/download/20190907/OpenAL-CS.dll.config
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/OpenAL-CS.dll
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/OpenAL-CS.dll.config
 	fi
 fi
 
 if [ ! -f Eluant.dll ]; then
-	echo "Fetching Eluant from GitHub."
+	echo "Fetching Eluant from Gitee."
 	if command -v curl >/dev/null 2>&1; then
-		curl -s -L -O https://github.com/OpenRA/Eluant/releases/download/20160124/Eluant.dll
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/Eluant.dll
 	else
-		wget -cq https://github.com/OpenRA/Eluant/releases/download/20160124/Eluant.dll
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/Eluant.dll
 	fi
 fi
 
 if [ ! -f rix0rrr.BeaconLib.dll ]; then
-	echo "Fetching rix0rrr.BeaconLib from NuGet."
-	../noget.sh rix0rrr.BeaconLib 1.0.1
-	cp ./rix0rrr.BeaconLib/lib/net40/rix0rrr.BeaconLib.dll .
-	rm -rf rix0rrr.BeaconLib
+	echo "Fetching rix0rrr.BeaconLib from Gitee."
+	if command -v curl >/dev/null 2>&1; then
+		curl -s -L -O https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/rix0rrr.BeaconLib.dll
+	else
+		wget -cq https://gitee.com/CastleJing/OpenRA_thirdparty_deps/raw/master/download/rix0rrr.BeaconLib.dll
+	fi
 fi
+
+chmod +x *.exe
+
