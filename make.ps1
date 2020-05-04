4c325c527f2986eb49dd89b7a718e28883f63285
CastleJing <castlejing@126.com>
2020-05-04 20:44:05 +0800

    <引擎><下载源分流>IP2LOCATION国内分流

--- make.ps1
+++ make.ps1
@@ -26,7 +26,7 @@ function All-Command
 	{
 		echo "Downloading IP2Location GeoIP database."
 		$target = Join-Path $pwd.ToString() "IP2LOCATION-LITE-DB1.IPV6.BIN.ZIP"
-		(New-Object System.Net.WebClient).DownloadFile("https://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.IPV6.BIN.ZIP", $target)
+		(New-Object System.Net.WebClient).DownloadFile("ftp://139.155.24.179/ip2location/IP2LOCATION-LITE-DB1.IPV6.BIN.ZIP", $target)
 	}
 }
 
