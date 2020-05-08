1f1a513cd2f7c2d16d75a3ebcfba7c0f6cce87a1
CastleJing <castlejing@126.com>
2020-05-08 10:11:51 +0800

    <引擎><文本修改>游戏内菜单任务目标文本修改

--- OpenRA.Mods.Common/Widgets/Logic/Ingame/GameInfoObjectivesLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Ingame/GameInfoObjectivesLogic.cs
@@ -44,7 +44,7 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 
 			var missionStatus = widget.Get<LabelWidget>("MISSION_STATUS");
 			missionStatus.GetText = () => player.WinState == WinState.Undefined ? "正在进行" :
-				player.WinState == WinState.Won ? "胜利" : "失败";
+				player.WinState == WinState.Won ? "完成" : "失败";
 			missionStatus.GetColor = () => player.WinState == WinState.Undefined ? Color.White :
 				player.WinState == WinState.Won ? Color.LimeGreen : Color.Red;
 
